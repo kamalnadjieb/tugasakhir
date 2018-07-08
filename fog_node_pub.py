fb389bd555560450654a63792617dd77ed2446b8
Muhammad Kamal Nadjieb <kamal.nadjieb@gmail.com>
2018-07-09 00:35:42 +0700

    add fog node

---
+++ fog_node_pub.py
@@ -0,0 +1,68 @@
+import time
+import paho.mqtt.publish as publish
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Hash import HMAC
+
+def get_nonce():
+    return str(int(time.time() * 1000000))
+
+def generate_msg(key, plaintext):
+    nonce = get_nonce()
+    cipher = AES.new(key, AES.MODE_CFB, nonce)
+    ciphertext = cipher.encrypt(plaintext)
+    hasher = HMAC.new(key, ciphertext, SHA256)
+    hash = hasher.hexdigest() 
+    msg = hash + nonce + ciphertext
+
+    return msg
+
+if __name__ == "__main__":
+    key = "Sixteen byte key"
+    expired_nonce = get_nonce()
+
+    # Light Off
+    msg = generate_msg(key, '0')
+    print "Send Authentic Light Off Message"
+    publish.single("things/1/light", msg, hostname="localhost")
+
+    # Wait for 1 second
+    print "Wait 1 second"
+    time.sleep(1)
+
+    # Light On
+    msg = generate_msg(key, '1')
+    print "Send Authentic Light On Message"
+    publish.single("things/1/light", msg, hostname="localhost")
+
+    # Wait for 1 second
+    print "Wait 1 second"
+    time.sleep(1)
+
+    # Fake Key
+    fake_key = "Sixteen byte keo"
+    msg = generate_msg(fake_key, '1')
+    print "Send Fake Key Light On Message"
+    publish.single("things/1/light", msg, hostname="localhost")
+
+    # Wait for 1 second
+    print "Wait 1 second"
+    time.sleep(1)
+
+    # Expired Nonce
+    cipher = AES.new(key, AES.MODE_CFB, expired_nonce)
+    ciphertext = cipher.encrypt('0')
+    hasher = HMAC.new(key, ciphertext, SHA256)
+    hash = hasher.hexdigest() 
+    msg = hash + expired_nonce + ciphertext
+    print "Send Expired Nonce Light Off Message"
+    publish.single("things/1/light", msg, hostname="localhost")
+
+    # Wait for 1 second
+    print "Wait 1 second"
+    time.sleep(1)
+
+    # Fake Key
+    msg = generate_msg(key, '09809098')
+    print "Send Fake Key Light On Message"
+    publish.single("things/1/light", msg, hostname="localhost")
---
+++ fog_node_sub.py
@@ -0,0 +1,50 @@
+import time
+import paho.mqtt.subscribe as subscribe
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Hash import HMAC
+
+last_light_message_nonce = None
+
+def get_current_time():
+    return int(time.time() * 1000000)
+
+def is_message_authentic(key, hash, nonce, ciphertext, last_nonce):
+    hasher = HMAC.new(key, ciphertext, SHA256)
+    if hasher.hexdigest() != hash:
+        # Key False
+        return 1
+    elif last_nonce != None and int(nonce) - int(last_nonce) < 0:
+        # Nonce Expired
+        return 2
+    else:
+        # True
+        return 0
+
+def on_message_light(client, userdata, message):
+    global last_light_message_nonce
+    key = "Sixteen byte key"
+    hash = message.payload[:64]
+    nonce = message.payload[64:80]
+    ciphertext = message.payload[80:]
+
+    message_authenticity = is_message_authentic(key, hash, nonce, ciphertext, last_light_message_nonce)
+    if message_authenticity == 1:
+        print "Message Not Authenticated - Key False"
+    elif message_authenticity == 2:
+        print "Message Not Authenticated - Nonce Expired"
+    elif message_authenticity == 0:
+        print "Message Authenticated"
+        last_light_message_nonce = nonce
+        cipher = AES.new(key, AES.MODE_CFB, nonce)
+        plaintext = cipher.decrypt(ciphertext)
+        if plaintext == '0':
+            print 'Light Off'
+        elif plaintext == '1':
+            print 'Light On'
+        else:
+            print 'Value Not Valid'
+    print
+
+if __name__ == "__main__":
+    subscribe.callback(on_message_light, "things/1/light", hostname="localhost")
